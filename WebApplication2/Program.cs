2c3f2cf2603495fb2ed0d350c425164ddacf6e1c
natra <natra@KURUL>
2024-02-07 12:06:06 +0200

    Добавьте файлы проекта.

---
+++ WebApplication2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Эта строка создает экземпляр WebApplicationBuilder, который используется для конфигурации приложения.
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Добавление сервисов в контейнер зависимостей:
+builder.Services.AddControllersWithViews();
+//Создание экземпляра WebApplication
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+     app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
+app.Run();
